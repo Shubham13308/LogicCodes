f52c5194713691662ae3bc9fc2a62f075c8325a2
Shubham Sharma <103982047+Shubham13308@users.noreply.github.com>
2022-10-04 22:13:53 +0530

    Reverse using loop

---
+++ src/Reverse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number to revere");
+
+        int n=sc.nextInt();
+        int rev=0,rem;
+        while(n!=0){
+            rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
+
+        }
+        System.out.println(rev);
+    }
+}
